1564ad2421833a74e7cdaed4260871f6b987e10b
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2017-07-20 20:28:05 -0700

    Updated jquery

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
 url += '?' + $.param({
   'api-key': "d0cb4a5c18674d3d8621efeb937e32d8"
 });
+
+
 $.ajax({
   url: url,
   method: 'GET',
 }).done(function(result) {
   console.log(result);
+
+  //Grab value from search box
+  
+
+
+
 }).fail(function(err) {
   throw err;
 });
