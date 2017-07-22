859c1440d46f2e8aedb101a16d45461ebd7b9053
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2017-07-21 17:51:32 -0700

    final version

--- app.js
+++ app.js
@@ -1,49 +1,67 @@
 
-  $("form").on( "submit", function( event ) {
+  $("form").on("submit", function( event ) {
 
     event.preventDefault();
+    // var formData = $('#search').val();
+    var formData = 'q=';
+    // formData = $(this).serialize();
+    var search = $('#search').val();
+    formData += search;
+    formData += '&';
+    var begin = $('#begin').val();
+    formData += 'begin_date';
+    formData += begin;
+    formData += '0101&';
+    var end = $('#end').val();
+    formData += 'end_date';
+    formData += end;
+    formData += '0101&';
+    var page = $('#page').val();
+    formData += page;
 
-    formData = $(this).serialize();
 
-    console.log(formData);
+    // console.log(formData);
 
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=d0cb4a5c18674d3d8621efeb937e32d8&" + formData;
+    var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=d0cb4a5c18674d3d8621efeb937e32d8&" + formData;
+    console.log('This is the url being called ' + url);
 
     $.ajax({
       url: url,
       method: 'GET',
+      header: {"Access-Control-Allow-Origin": "*"}
     }).done(function(result) {
       console.log(result);
+      var articles = result.response.docs;
+      for (var i = 0; i < articles.length; i++){
+
+        var div = $('<div>')
+          .addClass('articleDiv');
+        //Headline
+        var el = $('<div>')
+          .html('<h2>' + result.response.docs[i].headline.main +'</h2><h4>'+ result.response.docs[i].byline.original +'</h4>');
+        div.append(el);
+
+        el = $('<h5>')
+          .text(result.response.docs[i].section_name);
+        $('#results').append(el);
+        div.append(el);
+
+        el = $('<h6>')
+          .text(result.response.docs[i].pub_date);
+        $('#results').append(el);
+        div.append(el);
+
+        el = $('<a>')
+          .addClass('idontknow')
+          .attr('href', result.response.docs[i].web_url)
+          .text("Read Full Article");
+          div.append(el);
+
+        $('#results').append(div);
+
+      }
     }).fail(function(err) {
       throw err;
     });
 
-  });
-
-  var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=d0cb4a5c18674d3d8621efeb937e32d8&q=hackers";
-
-  $.ajax({
-    url: url,
-    method: 'GET',
-  }).done(function(result) {
-
-
-    var articles = result.response.docs;
-    var headLine = result.response.docs[0].headline.main;
-    var author = result.response.docs[0].byline.original;
-    var opinion = result.response.docs[0].section_name;
-    var pub = result.response.docs[0].pub_date;
-    var link = result.response.docs[0].web_url;
-
-
-    console.log(result);
-    console.log(articles);
-    console.log(headLine);
-    console.log(author);
-    console.log(opinion);
-    console.log(pub);
-    console.log(link);
-
-  }).fail(function(err) {
-    throw err;
   });
